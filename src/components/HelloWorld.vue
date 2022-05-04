83d1320b8a61351c1b08d6b18d11b351ec36ae72
zhangguian <2027432474@qq.com>
2022-05-04 10:16:24 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import { sayHello } from '../api/hello'
+const count = ref(0);
+const message = ref('zz—music-admin')
+
+const hi = () => {
+  sayHello().then(res => {
+    console.log(res)
+  })
+  message.value = '一起'
+}
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+  <div>
+    <q-btn
+      color="primary"
+      label="Primary"
+      @click="count++"
+    >点击此时{{count}}</q-btn>
+    <q-btn
+      color="primary"
+      label="Primary"
+      @click="hi"
+    >点击提交{{count}}</q-btn>
+
+  </div>
+</template>
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const maApp = createApp(App);
+
+maApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+});
+
+maApp.mount('#app');
